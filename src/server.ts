9100714e4094684de3f8b8215f8c1a5775e5086b
laripeanuts <larissarabelolf@gmail.com>
2023-02-10 19:58:13 -0300

    feat: start project with eslint, commitzen, fastify, typescript

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get("/hello", (req, res) => {
+  return "Hello World";
+});
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("Server listening on port 3333");
+  });
