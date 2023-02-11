a4f433225b354c12371e8b7c27ee2cb7e9283ddc
laripeanuts <larissarabelolf@gmail.com>
2023-02-11 20:07:03 -0300

    feat: implements database routes

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,12 @@
 import fastify from "fastify";
-import crypto from "node:crypto";
-import { knex } from "./database";
 import { env } from "./env";
+import { transactionsRoutes } from "./routes/transactions";
+import cookie from "@fastify/cookie";
 
 const app = fastify();
 
-app.get("/transactions", async (req, res) => {
-  const transaction = await knex("transactions").select("*");
-
-  return transaction;
-});
-
-app.post("/transactions", async (req, res) => {
-  const transaction = await knex("transactions")
-    .insert({
-      id: crypto.randomUUID(),
-      title: "Test Transaction",
-      amount: 500,
-      session_id: crypto.randomUUID(),
-    })
-    .returning("*");
-
-  return transaction;
-});
+app.register(cookie);
+app.register(transactionsRoutes, { prefix: "transactions" });
 
 app
   .listen({
