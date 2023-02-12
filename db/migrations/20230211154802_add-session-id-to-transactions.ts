0513978eb463ecd7b94009d342cb22b5c73866b2
laripeanuts <larissarabelolf@gmail.com>
2023-02-12 15:43:30 -0300

    fix: updates migrations

--- db/migrations/20230211154802_add-session-id-to-transactions.ts
+++ db/migrations/20230211154802_add-session-id-to-transactions.ts
@@ -2,7 +2,7 @@ import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable("transactions", (table) => {
-    table.uuid("session_id").after("id").index().notNullable();
+    table.uuid("session_id").after("id").index();
   });
 }
 
